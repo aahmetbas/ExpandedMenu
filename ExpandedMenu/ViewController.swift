8cd0281c0f1d233e7713d6d9458a62ffd9740307
Alp <aahmetbas@gmail.co>
2019-01-29 14:09:16 +0300

    Initial Commit

---
+++ ExpandedMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExpandedMenu
+//
+//  Created by Alperen Ahmet Baş on 29.01.2019.
+//  Copyright © 2019 Alperen Ahmet Baş. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ExpandedMenuTests/ExpandedMenuTests.swift
@@ -0,0 +1,34 @@
+//
+//  ExpandedMenuTests.swift
+//  ExpandedMenuTests
+//
+//  Created by Alperen Ahmet Baş on 29.01.2019.
+//  Copyright © 2019 Alperen Ahmet Baş. All rights reserved.
+//
+
+import XCTest
+@testable import ExpandedMenu
+
+class ExpandedMenuTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
